9e14d0682afe942bf9164ba79bc6603eb0fd5422
Jihyeon An <jh011007@naver.com>
2023-05-21 04:22:08 +0900

    refactor : 색상 변경

--- app/src/main/java/sopt/sopkathon/weing/presentation/kill/KillActivity.kt
+++ app/src/main/java/sopt/sopkathon/weing/presentation/kill/KillActivity.kt
@@ -1,12 +1,32 @@
 package sopt.sopkathon.weing.presentation.kill
 
+import android.content.Intent
 import android.os.Bundle
 import sopt.sopkathon.weing.R
 import sopt.sopkathon.weing.databinding.ActivityKillBinding
+import sopt.sopkathon.weing.databinding.DialogVoteBinding
 import sopt.sopkathon.weing.presentation.base.BindingActivity
+import sopt.sopkathon.weing.presentation.home.HomeActivity
 
 class KillActivity : BindingActivity<ActivityKillBinding>(R.layout.activity_kill) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //이미지 불러오기
+        loadImage()
+
+        //toolbar 백버튼
+        binding.toolbar.btnBack.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        //이미지 재로딩
+        binding.btnKillReload.setOnClickListener {
+            loadImage()
+        }
     }
+
+    private fun loadImage() {
+
+    }
+
 }
